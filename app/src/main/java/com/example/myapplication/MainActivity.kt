3efadf287f0c26b0d604bbcd756d07317039d1ee
cckn <cckn1985@gmail.com>
2019-04-21 18:08:18 +0900

    챕터 3 완료  - 기본적인 레이아웃  - 버튼 추가  - 클릭 리스너

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.myapplication
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() { // AppCompatActivity 상속
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState) // 오버라이드 시 반드시 부모 클래스의 생성자를 호출 할 것
         setContentView(R.layout.activity_main)
+
+        clickButton.setOnClickListener{
+            textView.text = "버튼을 눌렀습니다"
+        }
     }
 }
